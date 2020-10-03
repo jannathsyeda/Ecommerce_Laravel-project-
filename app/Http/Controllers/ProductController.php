0fdcad0c5a8a190546236fdee9d5c6cee94b5af4
jannath <jannathsyeda7@gmail.com>
2020-10-04 00:02:35 +0600

    single product add to cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,5 +26,46 @@ class ProductController extends Controller
 
     	return view('pages.product_details',compact('product','product_color','product_size'));
 
-    }
+	}
+	
+	public function AddCart(Request $request, $id){
+		$product = DB::table('products')->where('id',$id)->first();
+		
+		  $data = array();
+		 
+		 if ($product->discount_price == NULL) {
+			 $data['id'] = $product->id;
+			 $data['name'] = $product->product_name;
+			 $data['qty'] = $request->qty;
+			 $data['price'] = $product->selling_price;
+			 $data['weight'] = 1;
+			 $data['options']['image'] = $product->image_one;
+			 $data['options']['color'] = $request->color;
+			 $data['options']['size'] = $request->size;
+			  Cart::add($data);
+			 $notification=array(
+								'messege'=>'Product Successfully Added',
+								'alert-type'=>'success'
+								 );
+							   return Redirect()->back()->with($notification);
+		 }else{
+		
+			 $data['id'] = $product->id;
+			 $data['name'] = $product->product_name;
+			 $data['qty'] = $request->qty;
+			 $data['price'] = $product->discount_price;
+			 $data['weight'] = 1;
+			 $data['options']['image'] = $product->image_one;
+			 $data['options']['color'] = $request->color;
+			 $data['options']['size'] = $request->size;
+			  Cart::add($data);
+			  $notification=array(
+								'messege'=>'Product Successfully Added',
+								'alert-type'=>'success'
+								 );
+							   return Redirect()->back()->with($notification);
+		
+			}   
+		
+			  }
 }
